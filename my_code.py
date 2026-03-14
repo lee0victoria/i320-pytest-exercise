bf26638936b00fa7b5b2a10e5916a1582fbd6237
Victoria Lee <lee0victoria@utexas.edu>
2026-03-13 23:56:30 -0500

    Added tests for additional valid inputs and invalid/malformed numbers (currently failing)

--- my_code.py
+++ my_code.py
@@ -1,14 +1,24 @@
-import pytest
-
 def fix_phone_num(phone_num_to_fix):
-  # given "5125558823". Split the parts, then recombine and return
+  # given "5125555678". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
-  four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
+  four_part = phone_num_to_fix[6:] # # 5678 (last four digits)
   
   fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
   
   return fixed_num
 
-def test_fix_phone_num():
-  assert fix_phone_num("5125558823") == '(512) 555 8823'
+def test_fix_phone_num_valid():
+    assert fix_phone_num("555-442-9876") == "5554429876"
+    
+    # New assertions
+    assert fix_phone_num("5554429876") == "5554429876"
+    assert fix_phone_num("3216543333") == "3216543333"
+    
+def test_fix_phone_num_too_long_or_misformatted():
+    import pytest
+    # These should raise errors once function is fixed
+    with pytest.raises(ValueError):
+        fix_phone_num("555-442-98761")
+    with pytest.raises(ValueError):
+        fix_phone_num("(3213) 654 3333")
